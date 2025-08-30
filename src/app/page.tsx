4351acfae9abbd7a6a312acd806411ae40ca2f87
ManavSaini124 <itsmemanavsaini@gmail.com>
2025-08-30 18:12:55 +0530

    inngest setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,21 @@
+"use client"
 
-import {  getQueryClient, trpc } from "@/trpc/server"
-import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
-import { Client } from "./client";
-import { Suspense } from "react";
-const page= async()=> {
-  const quearyClient = getQueryClient();
-  void quearyClient.prefetchQuery(trpc.create.queryOptions({text:"Manav Prefetch"}));
-  // const trpc = useTRPC();
-  // const { data } = useQuery(trpc.create.queryOptions({text:"Manav"})); // similar to => api/trpc/body={"text":"Manav"}
+import { Button } from "@/components/ui/button"
+import { useTRPC } from "@/trpc/client"
+import { useMutation } from "@tanstack/react-query";
+
+const page= ()=> {
+  const trpc = useTRPC();
+  const invoke = useMutation(trpc.invoke.mutationOptions({}))
+
   return (
-    <HydrationBoundary state={dehydrate(quearyClient)}>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Client />
-      </Suspense>
-    </HydrationBoundary>
+    <div className="flex flex-col gap-2">
+      <Button
+        onClick={()=> invoke.mutate({ text: "manav" })}
+      >
+        Invoke
+      </Button>
+    </div>
   )
 }
 
