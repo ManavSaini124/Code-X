e557b859957c6adc003462160ba9be3bb65b961e
ManavSaini124 <itsmemanavsaini@gmail.com>
2025-09-01 12:30:12 +0530

    feat: add messaging functionality with Prisma models and TRPC procedures

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,24 +3,32 @@
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input";
 import { useTRPC } from "@/trpc/client"
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { useState } from "react";
+import { toast } from "sonner";
 
-const page= ()=> {
+const Page= ()=> {
   const [value, setValue] = useState("")
   const trpc = useTRPC();
-  const invoke = useMutation(trpc.invoke.mutationOptions({}))
+  const { data: messages } = useQuery(trpc.messages.getMany.queryOptions());
+  const createMessage = useMutation(trpc.messages.create.mutationOptions({
+    onSuccess: ()=>{
+      toast.success("Message created")
+    }
+  }))
 
   return (
     <div className="flex flex-col gap-2">
       <Input value={value} onChange={(e)=> setValue(e.target.value)} />
       <Button
-        onClick={()=> invoke.mutate({ Value: value })}
+        onClick={()=> createMessage.mutate({ value: value })}
       >
         Invoke
       </Button>
+
+      {JSON.stringify(messages, null, 2)}
     </div>
   )
-}
+}  
 
-export default page
+export default Page
