42d9cf2a6c22f8db3f410b4433bc0faa79a53805
ManavSaini124 <itsmemanavsaini@gmail.com>
2025-09-01 23:22:05 +0530

    feat: enhance messaging system with theme support and loading indicators

--- src/modules/projects/ui/views/project-view.tsx
+++ src/modules/projects/ui/views/project-view.tsx
@@ -8,14 +8,16 @@ import {
     ResizableHandle
 } from "@/components/ui/resizable"
 import { MessagesContainer } from "../components/messages-container";
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
+import { Fragment } from "@/generated/prisma";
+import { ProjectHeader } from "../components/project-header";
 
 interface Props {
     projectId: string
 };
 
 export const ProjectView = ({ projectId }: Props) => {
-    
+    const [activeFragment, setActiveFragment] = useState<Fragment | null>(null);
     return (
         <div className="h-screen">
             <ResizablePanelGroup direction="horizontal">
@@ -25,7 +27,14 @@ export const ProjectView = ({ projectId }: Props) => {
                 className="flex flex-col min-h-0"
             >
                 <Suspense fallback={<div>Loading...</div>}>
-                    <MessagesContainer projectId={projectId} />
+                    <ProjectHeader projectId={projectId} />
+                </Suspense>
+                <Suspense fallback={<div>Loading...</div>}>
+                    <MessagesContainer 
+                        projectId={projectId} 
+                        activeFragment={activeFragment}
+                        setActiveFragment={setActiveFragment}
+                    />
                 </Suspense>
             </ResizablePanel>
             <ResizableHandle withHandle />
